205492e42aa46b98352588e9b0d2f46d19d24056
Hussein Dimessi <15121126+hdimessi@users.noreply.github.com>
2021-12-31 00:39:15 +0100

    Add Navbar

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,10 +1,13 @@
 import React, {Component} from "react";
 import ReactDom from "react-dom";
+import Header from "./layout/Header";
 
 class App extends Component {
 
     render() {
-        return <h1>React App</h1>
+        return (
+            <Header />
+        )
     }
 }
 
